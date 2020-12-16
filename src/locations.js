c30dd3839dc3aed50adc0c977d049b2ea9b02aaf
Alyssa <alyssasgable@gmail.com>
2020-12-16 12:53:09 -0500

    updated locations.txt

--- src/locations.js
+++ src/locations.js
@@ -3,7 +3,7 @@ const NodeGeocoder = require("node-geocoder");
 
 const options = {
   provider: "google",
-  httpAdapter: "http",
+  httpAdapter: "https",
   apiKey: process.env.GOOGLE_API_KEY,
   sensor: true
 };
